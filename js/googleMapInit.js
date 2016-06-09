55f7bf53d9f0f85d44b1232b720c31eb94d771b4
Fuzzycows Inc <junk@fuzzycows.com>
2016-06-09 15:58:51 -0400

    ,,,

--- js/googleMapInit.js
+++ js/googleMapInit.js
@@ -7,7 +7,7 @@ function initialize() {
     var map_canvas = document.getElementById('googleMap');
 
     var map_options = {
-        center: new google.maps.LatLng(44.434596, 26.080533),
+        center: new google.maps.LatLng(43.6321615, -79.6641183),
         zoom: 16,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         scrollwheel: false
@@ -15,76 +15,18 @@ function initialize() {
 
     var map = new google.maps.Map(map_canvas, map_options);
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(44.434596, 26.080533),
+        position: new google.maps.LatLng(43.6321615, -79.6641183),
         map: map,
-        title: 'Hello World!'
+        title: 'Our Address!'
     });
     var styles = [
-        {
-            "featureType": "landscape",
-            "stylers": [
-                { "visibility": "on" },
-                { "color": "#282828" }
-            ]
-        },{
-            "featureType": "poi",
-            "stylers": [
-                { "visibility": "off" }
-            ]
-        },{
-            "featureType": "road",
-            "stylers": [
-                { "color": "#383838" }
-            ]
-        },{
-            "elementType": "geometry.stroke",
-            "stylers": [
-                { "visibility": "off" }
-            ]
-        },{
-            "featureType": "poi",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                { "visibility": "on" },
-                { "weight": 8 },
-                { "hue": "#ff0000" },
-                { "color": "#ffffff" }
-            ]
-        },{
-            "featureType": "landscape",
-            "elementType": "labels.text.stroke",
-            "stylers": [
-                { "color": "#ffffff" },
-                { "visibility": "on" }
-            ]
-        },{
-            "featureType": "poi",
-            "elementType": "labels.icon",
-            "stylers": [
-                { "visibility": "on" }
-            ]
-        },{
-            "featureType": "water",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                { "visibility": "off" },
-                { "color": "#ffffff" }
-            ]
-        },{
-            "featureType": "water",
-            "elementType": "labels.text.stroke",
-            "stylers": [
-                { "visibility": "on" },
-                { "color": "#ffffff" }
-            ]
-        },{
-            "featureType": "water",
-            "stylers": [
-                { "color": "#004044" }
-            ]
-        },{
-        }
+  {
+    "stylers": [
+      { "hue": "#ecb205" },
+      { "saturation": 1 }
     ]
+  }
+];
     map.setOptions({styles: styles});
 }
 google.maps.event.addDomListener(window, 'load', initialize);
